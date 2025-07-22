3dbb47ccc81692dfed2ff2c17df5909806177f21
nicolas-ferrada <118396552+nicolas-ferrada@users.noreply.github.com>
2025-07-22 00:34:12 -0400

    feat: add appId and nameLabel

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.coin_and_claw"
+    namespace = "com.nicolasferrada.coin_and_claw"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.coin_and_claw"
+        applicationId = "com.nicolasferrada.coin_and_claw"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
